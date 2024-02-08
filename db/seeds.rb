036f6314e05d08f80986d2369c58b19ce2778424
Korblen <malo.bastianelli@gmail.com>
2024-02-08 19:09:07 +0100

    enfifini

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,51 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+cities = ["Paris", "Lyon", "Marseille", "Toulouse", "Nice"]
+cities.each do |name|
+  City.create!(name: name)
+end
+
+# Créer des spécialités
+specialties = ["Cardiologie", "Dermatologie", "Pédiatrie", "Neurologie"]
+specialties.each do |name|
+  Specialty.create!(name: name)
+end
+
+# Créer des docteurs
+10.times do
+  Doctor.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    zip_code: Faker::Address.zip_code,
+    city: City.order("RANDOM()").first
+  )
+end
+
+# Créer des patients
+20.times do
+  Patient.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    city: City.order("RANDOM()").first
+  )
+end
+
+# Assigner des spécialités aux docteurs
+Doctor.all.each do |doctor|
+  specialties_sample = Specialty.order("RANDOM()").limit(rand(1..3))
+  doctor.specialties << specialties_sample
+end
+
+# Créer des rendez-vous
+50.times do
+  Appointment.create!(
+    date: Faker::Time.forward(days: 23, period: :day),
+    doctor: Doctor.order("RANDOM()").first,
+    patient: Patient.order("RANDOM()").first,
+    city: City.order("RANDOM()").first
+  )
+end
+
+puts "Données générées avec succès !"
